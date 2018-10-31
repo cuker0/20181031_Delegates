ba58e65de37bd667555e7a58afc448aa4f6dc7c6
Marcin Cukrowski <marcin.cukrowski@balluff.pl>
2018-10-31 10:58:01 +0100

    delegaty add, substract

--- 20181031_Delegates/Program.cs
+++ 20181031_Delegates/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace _20181031_Delegates
 {
-    class Program
+   
+    static class Program
     {
         static void Main(string[] args)
         {
+            
+            SimpleDelegateTest();
+          
+            Console.ReadKey();          
+        }
+
+        private static void SimpleDelegateTest()
+        {
+            MathDelegate del = new MathDelegate(Math.Add); //stworzenie domyslnej delegaty
+            callDelegate(del);
+            del += Math.Substract; // przypisane kolejnej metody do delegaty
+            callDelegate(del);
+
+            del = null; // wyczyszczenie delegat
+            del?.Invoke(0,0);
+            callDelegate(del);
+        }
+
+        private static void callDelegate(MathDelegate del)
+        {
+            Console.WriteLine("################################");
+            del(10, 2);
         }
     }
 }
